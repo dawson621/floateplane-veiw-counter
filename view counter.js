fbadcb87ac2b2c8caccf8dc1eaf7f9a4ed23d726
dawson621 <122133472+dawson621@users.noreply.github.com>
2023-01-06 19:58:58 -0500

    Create view counter.js

---
+++ view counter.js
@@ -0,0 +1,21 @@
+function refreshList() {
+	var btn = document.querySelectorAll(`.chat-view-container.chat-messages-container:not(.hide) .header-button.btn-user-list, .chat-view-container.chat-users-container:not(.hide) .header-button.btn-user-list.btn-return-to-chat`)[0]
+	
+	if (btn) {btn.click()};
+}
+
+window.setInterval(function() {
+    var subTitle = document.getElementById("addedSh*t");
+    if (!subTitle) {
+        subTitle = document.createElement("div");
+        subTitle = document.getElementsByClassName("video-title")[0].insertAdjacentElement("afterEnd", subTitle);
+        subTitle.outerHTML = `<div class="video-title" id="addedSh*t">
+    <span class="title-text" style="font-size:12px" id="viewCount">View count: test</span>
+    </div>`;
+    }
+
+	refreshList();
+    document.getElementById("viewCount").innerText = "View count: " + document.querySelectorAll(".chat-viewer-name.ng-star-inserted").length;
+	refreshList();
+    //console.log(document.querySelectorAll(".chat-viewer-name.ng-star-inserted").length);
+}, secondsToRefresh * 1000);
